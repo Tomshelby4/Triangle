f4d71476ad5477b43255e1f9e5511007e297eadd
Tomshelby4 <brianmaera@gmail.com>
2018-08-10 16:38:51 +0300

    define variable c

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 function triangle(){
 var A = parseFloat(document.getElementById('sideOne').value);
 var B = parseFloat(document.getElementById('sideTwo').value);
+var C = parseFloat(document.getElementById('sideThree').value);
