b6de63cf07e837989bd07e03ccf2ccc279e3baf0
Tomshelby4 <brianmaera@gmail.com>
2018-08-10 16:45:37 +0300

    arithmetic for triangles which will not form

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ function triangle(){
 var A = parseFloat(document.getElementById('sideOne').value);
 var B = parseFloat(document.getElementById('sideTwo').value);
 var C = parseFloat(document.getElementById('sideThree').value);
+/*make variables to count for triangles which will not form*/
+	var AB = A + B;
+  var AC = A + C;
+  var BC = B + C;
